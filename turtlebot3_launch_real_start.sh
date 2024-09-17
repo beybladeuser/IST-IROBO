269a7a28e34f350cce9653dcba926c3410e98b3f
beybladeuser <rubencruz0312@gmail.com>
2024-09-17 09:53:18 +0100

    even more commands

---
+++ turtlebot3_launch_real_start.sh
@@ -0,0 +1,8 @@
+source ./turtlebot3_launch_real_setup.sh
+ssh user@$TURTLEBOT3_IP
+roscore
+#in another terminal
+ssh user@$TURTLEBOT3_IP
+sudo apt-get install ntpdate #optional
+sudo ntpdate ntp.ubuntu.com #optional
+roslaunch turtlebot3_bringup turtlebot3_robot.launch
