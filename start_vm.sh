e3101bf8c512fe3ee879ae2c006cc8c00817ee67
beybladeuser <rubencruz0312@gmail.com>
2024-09-27 17:28:38 +0100

    updated start_vm

--- start_vm.sh
+++ start_vm.sh
@@ -1,25 +1,27 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install build-essential curl git wget -y
-sudo apt install net-tools
+sudo apt install net-tools -y
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-noetic-desktop-full
+sudo apt install ros-noetic-desktop-full -y
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
-sudo apt install python3-rosdep
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+sudo apt install python3-rosdep -y
 sudo rosdep init
 rosdep update
-sudo apt-get install ros-noetic-turtlebot3-msgs
-#sudo apt-get install ros-noetic-turtlebot3-gazebo
-sudo apt-get install ros-noetic-turtlebot3
-sudo apt-get install ros-noetic-robot-localization
-sudo apt-get install ros-noetic-dynamixel-sdk
-sudo apt-get install ros-noetic-teleop-twist-keyboard
-sudo apt-get install ros-noetic-gmapping
-sudo apt-get install ros-noetic-navigation
+sudo apt-get install ros-noetic-turtlebot3-msgs -y
+#sudo apt-get install ros-noetic-turtlebot3-gazebo -y
+sudo apt-get install ros-noetic-turtlebot3 -y
+sudo apt-get install ros-noetic-robot-localization -y
+sudo apt-get install ros-noetic-dynamixel-sdk -y
+sudo apt-get install ros-noetic-teleop-twist-keyboard -y
+sudo apt-get install ros-noetic-gmapping -y
+sudo apt-get install ros-noetic-navigation -y
 
+git submodule init
+git submodule update
 cd $(pwd)/catkin_ws
 catkin_make
