03d9126e81820ba7ecee245ced8b8d6f46abf758
beybladeuser <rubencruz0312@gmail.com>
2024-10-06 17:38:37 +0100

    fixed small error with initial prints

--- catkin_ws/src/miniproj1_datasets/src/scripts/gen_graph.py
+++ catkin_ws/src/miniproj1_datasets/src/scripts/gen_graph.py
@@ -6,7 +6,10 @@ graph_values = []
 with open(os.path.expanduser("~/.ros/log/latest/error_calc-8-stdout.log"), "r") as f:
 	for line in f:
 		line = re.sub(token_to_rm_regex, "", line.strip())
+		if "error" not in line.lower():
+			continue
 		line_values = line.split(" ")
+
 		temp = {
 			"x": line_values[2], #sim_time
 			"y": line_values[6], #error
