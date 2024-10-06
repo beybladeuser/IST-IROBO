2d50359fbdfa72e211e9147e847909c4812db7f6
beybladeuser <rubencruz0312@gmail.com>
2024-10-06 17:36:31 +0100

    fixed error with path

--- catkin_ws/src/miniproj1_datasets/src/scripts/gen_graph.py
+++ catkin_ws/src/miniproj1_datasets/src/scripts/gen_graph.py
@@ -1,12 +1,12 @@
 import re
+import os
 
 token_to_rm_regex = "[\[\],:\(\)]"
 graph_values = []
-with open("~/.ros/log/latest/error_calc-8-stdout.log", "r") as f:
+with open(os.path.expanduser("~/.ros/log/latest/error_calc-8-stdout.log"), "r") as f:
 	for line in f:
 		line = re.sub(token_to_rm_regex, "", line.strip())
 		line_values = line.split(" ")
-
 		temp = {
 			"x": line_values[2], #sim_time
 			"y": line_values[6], #error
