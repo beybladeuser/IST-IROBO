220ce38d246193d53809a018a0497556f7ef6c00
beybladeuser <rubencruz0312@gmail.com>
2024-10-06 18:03:15 +0100

    finished gen_graph

--- catkin_ws/src/miniproj1_datasets/src/scripts/gen_graph.py
+++ catkin_ws/src/miniproj1_datasets/src/scripts/gen_graph.py
@@ -1,10 +1,14 @@
 import re
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
+filename = "~/.ros/log/latest/error_calc-8-stdout.log"
+filename = "temp.txt"
 token_to_rm_regex = "[\[\],:\(\)]"
 graph_values = []
 start_time = None
-with open(os.path.expanduser("~/.ros/log/latest/error_calc-8-stdout.log"), "r") as f:
+with open(os.path.expanduser(filename), "r") as f:
 	for line in f:
 		line = re.sub(token_to_rm_regex, "", line.strip())
 		line_values = line.split(" ")
@@ -13,8 +17,17 @@ with open(os.path.expanduser("~/.ros/log/latest/error_calc-8-stdout.log"), "r") 
 		
 		if start_time == None:
 			start_time = float(line_values[2])
-		temp = {
-			"x": float(line_values[2]) - start_time, #sim_time
-			"y": float(line_values[6]), #error
-		}
-		graph_values.append(temp)
+
+		sim_time = float(line_values[2]) - start_time
+		error = float(line_values[6])
+
+		graph_values.append((sim_time, error))
+
+graph_values = np.array(graph_values)
+
+plt.plot(graph_values[:, 0], graph_values[:, 1], marker='o')
+plt.title('Simple Line Graph')
+plt.xlabel('Simulation Time')
+plt.ylabel('Error')
+plt.grid()
+plt.show()
