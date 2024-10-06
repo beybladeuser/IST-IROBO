582e111560a561ee2cc6339e32fc2a73c1e03c35
beybladeuser <rubencruz0312@gmail.com>
2024-10-06 17:48:03 +0100

    finished error extraction on gen_graph

--- catkin_ws/src/miniproj1_datasets/src/scripts/gen_graph.py
+++ catkin_ws/src/miniproj1_datasets/src/scripts/gen_graph.py
@@ -3,16 +3,18 @@ import os
 
 token_to_rm_regex = "[\[\],:\(\)]"
 graph_values = []
+start_time = None
 with open(os.path.expanduser("~/.ros/log/latest/error_calc-8-stdout.log"), "r") as f:
 	for line in f:
 		line = re.sub(token_to_rm_regex, "", line.strip())
-		if "error" not in line.lower():
-			continue
 		line_values = line.split(" ")
-
+		if "error" != line_values[3].lower():
+			continue
+		
+		if start_time == None:
+			start_time = float(line_values[2])
 		temp = {
-			"x": line_values[2], #sim_time
-			"y": line_values[6], #error
+			"x": float(line_values[2]) - start_time, #sim_time
+			"y": float(line_values[6]), #error
 		}
-		print(temp)
 		graph_values.append(temp)
