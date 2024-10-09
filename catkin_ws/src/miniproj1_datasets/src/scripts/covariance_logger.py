7d51bc6efa60c799e88e83dbd7f67e30f8e7bb64
beybladeuser <rubencruz0312@gmail.com>
2024-10-09 01:15:36 +0100

    changed approach for covariance logger

--- catkin_ws/src/miniproj1_datasets/src/scripts/covariance_logger.py
+++ catkin_ws/src/miniproj1_datasets/src/scripts/covariance_logger.py
@@ -4,15 +4,13 @@ import rospy
 import tf
 from nav_msgs.msg import Odometry
 from rosgraph_msgs.msg import Clock
-from miniproj1_utils import expand_filename, split_extension_from_filename, get_dup_file_index, create_file
 import os
-import pandas as pd
+import numpy as np
 import sys
 
 class CovarianceLogger:
-	def __init__(self, output, topic_name = '/frame_path'):
-		self.output = output
-		self.covariance = pd.DataFrame()
+	def __init__(self, topic_name = '/frame_path'):
+		self.covariance = None
 		subbed = False
 		while not subbed:
 			rospy.loginfo("Waiting for topic")
@@ -22,24 +20,15 @@ class CovarianceLogger:
 			except:
 				pass
 	def pose_callback(self, data):
-		self.covariance = pd.concat([self.covariance, pd.DataFrame([data.pose.covariance]).rename_axis("id") ], ignore_index=True)
+		self.covariance = np.array(data.pose.covariance)
 
 	def log(self):
-		try:
-			self.covariance.to_csv(self.output)		
-		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-			pass
+		rospy.loginfo("Cov: " + str(self.covariance))
 
 if __name__ == '__main__':
 	rospy.init_node('covariance_logger')
 	topic_name = rospy.get_param('~topic_name', '/odometry/filtered')  # The reference frame to track against
 	rate_num = rospy.get_param('~rate', 10)  # The reference frame to track against
-	output_filename = rospy.get_param('~output', './covariance')  # The reference frame to track against
-	output_filename = expand_filename(output_filename)
-	output_filename = split_extension_from_filename(output_filename)[0]
-	output_index = get_dup_file_index(output_filename)
-	output_filename = f"${output_filename}_${output_index}.csv"
-	create_file(output_filename)
 
 	if rospy.rostime.is_wallclock():
 		rospy.logfatal('You should be using simulated time: rosparam set use_sim_time true')
